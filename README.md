a5863a42931577cc548f8cd34559a313ceaff6a4
ayushikherli123 <36254291+ayushikherli123@users.noreply.github.com>
2020-02-24 18:00:31 +0530

    Updated tool and README

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PylintErrorCustomizer
-Tool is used to customize pylint errors
+Tool is used to customize pylint errors.
+
+Detailed tool description is given in pylint_error_customizer_manual.pdf
+
+A test.py script is also provided to simulate PylintErrorCustomizer tool usage
+
+
+Disclaimer : This tool is good for less number of python code files. This tool usage OS resources, so using bunch of files may lead to a longer execution.
--- pylint_error_customizer.py
+++ pylint_error_customizer.py
@@ -5,9 +5,12 @@ Date : 15 Feb 2020
 """
 
 import os
+import re
 import subprocess
 
 OUTPUT_FILE = 'pylint_errors.txt'
+FILTER_OUTPUT_FILE = 'filered_pylint_errors.txt'
+PYLINT_VERSION = "pylint3"
 
 class PylintErrorCustomizer:
     """
@@ -17,12 +20,14 @@ class PylintErrorCustomizer:
         if not isinstance(base_dir, str):
             raise TypeError("Invalid type, must be str")
         if not os.path.exists(base_dir):
-            raise FileNotFoundError("Invalid path, no such file or directory")
+            raise FileNotFoundError("Invalid path, no such file or directory : ", base_dir)
         self.__base_dir = base_dir
         self.__py_files = self.__find_py_files(base_dir)
 
     def __find_py_files(self, path):
-        self.__py_files = tuple([file_ for file_ in os.listdir(path) if file_.endswith(".py")])
+        self.__py_files = tuple([os.path.join(self.__base_dir, file_) \
+                                 for file_ in os.listdir(path) \
+                                 if file_.endswith(".py")])
         return self.__py_files
 
     def __validate_files(self, py_files):
@@ -48,7 +53,9 @@ class PylintErrorCustomizer:
         py_files = self.__validate_files(py_files)
         with open(OUTPUT_FILE, 'w') as out_file:
             for file_name in py_files:
-                out = subprocess.Popen(["pylint3", file_name],
+                if not os.path.exists(file_name):
+                    raise FileNotFoundError("Invalid path, no such file or directory : ", file_name) 
+                out = subprocess.Popen([PYLINT_VERSION, file_name],
                                        stdout=subprocess.PIPE,
                                        stderr=subprocess.PIPE)
                 stdout, stderr = out.communicate()
@@ -58,5 +65,12 @@ class PylintErrorCustomizer:
                 out_file.write('\n')
 
     def filter_pylint_errors(self, pattern):
-        """ Functionality will be added soon """
-        pass
+        """
+        Method is used to return filtered pylint errors in a text file
+        """
+        if not os.path.exists(OUTPUT_FILE):
+            raise FileNotFoundError("`pylint_errors.txt` file does not exist")
+        with open(OUTPUT_FILE, 'r') as in_file, open(FILTER_OUTPUT_FILE, 'a') as out_file:
+            for line_ in in_file:
+               if re.search(pattern, line_):
+                   out_file.write(line_+'\n')
